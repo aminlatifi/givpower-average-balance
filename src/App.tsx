d455c11ee00eaa9eb741348876e205c5a7a80168
Amin Latifi <a.latifi.al@gmail.com>
2022-09-04 18:21:10 +0430

    Open help links in new tab

--- src/App.tsx
+++ src/App.tsx
@@ -9,105 +9,110 @@ import config from './config';
 import { calculateAverage } from './lib/averageCalculator';
 
 function App() {
-  const onFinish = (values: any) => {
-    calculateAverage(
-      values.env,
-      values.walletAddress,
-      Number(values.fromTimestamp),
-      Number(values.toTimestamp),
-    ).then(setResult);
-  };
+	const onFinish = (values: any) => {
+		calculateAverage(
+			values.env,
+			values.walletAddress,
+			Number(values.fromTimestamp),
+			Number(values.toTimestamp),
+		).then(setResult);
+	};
 
-  const onFinishFailed = (errorInfo: any) => {
-    console.log('Failed:', errorInfo);
-  };
+	const onFinishFailed = (errorInfo: any) => {
+		console.log('Failed:', errorInfo);
+	};
 
-  const [result, setResult] = useState('');
+	const [result, setResult] = useState('');
 
-  const { Option } = Select;
+	const { Option } = Select;
 
-  return (
-    <div className='App'>
-      <h1>Calculate average GIVpower</h1>
+	return (
+		<div className='App'>
+			<h1>Calculate average GIVpower</h1>
 
-      <Form
-        name='basic'
-        labelCol={{ span: 7 }}
-        wrapperCol={{ span: 20 }}
-        initialValues={{ remember: true }}
-        onFinish={onFinish}
-        onFinishFailed={onFinishFailed}
-      >
-        <Form.Item
-          label='Environment'
-          name='env'
-          rules={[{ required: true, message: 'Please select the env!' }]}
-        >
-          <Select style={{ width: '100%' }}>
-            {Object.entries(config.environments).map(([key, e]) => {
-              return (
-                <Option key={key} value={e.subgraphUrl}>
-                  {e.title}
-                </Option>
-              );
-            })}
-          </Select>
-        </Form.Item>
+			<Form
+				name='basic'
+				labelCol={{ span: 7 }}
+				wrapperCol={{ span: 20 }}
+				initialValues={{ remember: true }}
+				onFinish={onFinish}
+				onFinishFailed={onFinishFailed}
+			>
+				<Form.Item
+					label='Environment'
+					name='env'
+					rules={[{ required: true, message: 'Please select the env!' }]}
+				>
+					<Select style={{ width: '100%' }}>
+						{Object.entries(config.environments).map(([key, e]) => {
+							return (
+								<Option key={key} value={e.subgraphUrl}>
+									{e.title}
+								</Option>
+							);
+						})}
+					</Select>
+				</Form.Item>
 
-        <Form.Item
-          label='Wallet Address'
-          name='walletAddress'
-          rules={[{ required: true, message: 'Please input Wallet Address!' }]}
-        >
-          <Input style={{ width: '100%' }} />
-        </Form.Item>
+				<Form.Item
+					label='Wallet Address'
+					name='walletAddress'
+					rules={[{ required: true, message: 'Please input Wallet Address!' }]}
+				>
+					<Input style={{ width: '100%' }} />
+				</Form.Item>
 
-        <Form.Item
-          label='From Timestamp'
-          name='fromTimestamp'
-          rules={[
-            {
-              required: true,
-              message: 'Please input timestamp (seconds) for the start of the period!',
-            },
-          ]}
-        >
-          <div style={{ display: 'flex', justifyContent: 'space-around' }}>
-            <InputNumber style={{ width: '70%' }} prefix={<ClockCircleOutlined />} />
-            <Tooltip title='Useful tool'>
-              <Typography.Link href='https://www.epochconverter.com/'>Need Help?</Typography.Link>
-            </Tooltip>
-          </div>
-        </Form.Item>
+				<Form.Item
+					label='From Timestamp'
+					name='fromTimestamp'
+					rules={[
+						{
+							required: true,
+							message:
+								'Please input timestamp (seconds) for the start of the period!',
+						},
+					]}
+				>
+					<div style={{ display: 'flex', justifyContent: 'space-around' }}>
+						<InputNumber style={{ width: '70%' }} prefix={<ClockCircleOutlined />} />
+						<Tooltip title='Useful tool'>
+							<Typography.Link href='https://www.epochconverter.com/' target="_blank">
+								Need Help?
+							</Typography.Link>
+						</Tooltip>
+					</div>
+				</Form.Item>
 
-        <Form.Item
-          label='To Timestamp'
-          name='toTimestamp'
-          rules={[
-            {
-              required: true,
-              message: 'Please input timestamp (seconds) for the end of the period!',
-            },
-          ]}
-        >
-          <div style={{ display: 'flex', justifyContent: 'space-around' }}>
-            <InputNumber style={{ width: '70%' }} prefix={<ClockCircleOutlined />} />
-            <Tooltip title='Useful tool'>
-              <Typography.Link href='https://www.epochconverter.com/'>Need Help?</Typography.Link>
-            </Tooltip>
-          </div>
-        </Form.Item>
+				<Form.Item
+					label='To Timestamp'
+					name='toTimestamp'
+					rules={[
+						{
+							required: true,
+							message: 'Please input timestamp (seconds) for the end of the period!',
+						},
+					]}
+				>
+					<div style={{ display: 'flex', justifyContent: 'space-around' }}>
+						<InputNumber style={{ width: '70%' }} prefix={<ClockCircleOutlined />} />
+						<Tooltip title='Useful tool'>
+							<Typography.Link href='https://www.epochconverter.com/' target="_blank">
+								Need Help?
+							</Typography.Link>
+						</Tooltip>
+					</div>
+				</Form.Item>
 
-        <Form.Item wrapperCol={{ offset: 4, span: 18 }}>
-          <Button type='primary' htmlType='submit'>
-            Submit
-          </Button>
-        </Form.Item>
-      </Form>
+				<Form.Item wrapperCol={{ offset: 4, span: 18 }}>
+					<Button type='primary' htmlType='submit'>
+						Submit
+					</Button>
+				</Form.Item>
+			</Form>
 
-      <pre style={{ textAlign: 'left' }}>{result}</pre>
-    </div>
-  );
+			<pre style={{ textAlign: 'left' }}>{result}</pre>
+		</div>
+	);
 }
 
 export default App;
